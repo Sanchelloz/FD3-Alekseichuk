ad7807f6c2bbbb7615f0da53190d3b39fcf78c26
Sanchelloz <alekseichuk.a@gmail.com>
2017-12-05 23:19:10 +0300

    create array

---
+++ ishop/VotesBlock.js
@@ -0,0 +1,36 @@
+﻿var VotesBlock = React.createClass({
+
+    displayName: 'VotesBlock',
+
+    propTypes: {
+        caption: React.PropTypes.string.isRequired,
+        //answers: React.PropTypes.array.isRequired,
+
+        catalog: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                name: React.PropTypes.string.isRequired,
+                URL: React.PropTypes.string.isRequired,
+                code: React.PropTypes.number.isRequired,
+                price: React.PropTypes.number.isRequired,
+                residue: React.PropTypes.number.isRequired,
+            })
+        )
+    },
+
+    render: function() {
+
+        var catalogCode = this.props.catalog.map(v =>
+            React.DOM.div({ key: v.name, className: 'Product_name' },
+                React.DOM.span({ className: 'Product_count' }, v.code),
+                React.DOM.span({ className: 'Product_price' }, v.price + 'руб'),
+                React.DOM.span({ className: 'Product_residue' }, v.residue),
+                //React.DOM.img({ className: 'Product_foto' }, v.URL),
+            )
+        );
+        return React.DOM.div({ className: 'VotesBlock' },
+            React.DOM.div({ className: 'Caption' }, this.props.caption),
+            React.DOM.div({ className: 'Catalog' }, catalogCode),
+        );
+    },
+
+});
