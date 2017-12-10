94e3303f2d7028868f254765f9f59c57f15425f0
Sanchelloz <alekseichuk.a@gmail.com>
2017-12-10 22:47:56 +0300

    1

---
+++ filter/JS/List.js
@@ -0,0 +1,8 @@
+var captionText = 'Введите текст для добавления в список';
+var listArr = [
+    { content: "здесь", code: 1 },
+    { content: "должна", code: 2 },
+    { content: "быть", code: 3 },
+    { content: "ваша", code: 4 },
+    { content: "реклама", code: 5 }
+];
---
+++ filter/JS/filter.js
@@ -0,0 +1,34 @@
+﻿var filter = React.createClass({
+
+    displayName: 'filterText',
+
+    listFilter: React.PropTypes.arrayOf(
+        React.PropTypes.shape({
+            content: React.PropTypes.string.isRequired,
+            code: React.PropTypes.number.isRequired,
+        })
+    )
+},
+
+
+render: function() {
+
+    var catalogCode = this.props.catalog.map(v =>
+        React.DOM.div({ key: v.name, className: 'Product' },
+            React.DOM.div({ className: 'Product_name' }, v.name),
+            React.DOM.div({ className: 'Product_foto' },
+                React.DOM.img({ src: v.URL }),
+            ),
+
+            React.DOM.div({ className: 'Product_count' }, v.code),
+            React.DOM.div({ className: 'Product_price' }, v.price + ' руб'),
+            React.DOM.div({ className: 'Product_residue' }, v.residue),
+
+        )
+    );
+    return React.DOM.div({ className: 'CatalogBlock' },
+        React.DOM.div({ className: 'Caption' }, this.props.caption),
+        React.DOM.div({ className: 'Catalog' }, catalogCode),
+    );
+},
+});
