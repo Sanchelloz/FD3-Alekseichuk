30832fed6eaf38dda788529dcf290898cdb9480f
Алексейчук Александр Николаевич <Alekseichuk.A@tbe.by>
2017-12-14 17:26:28 +0300

    add

--- filter/JS/filter.js
+++ filter/JS/filter.js
@@ -10,16 +10,30 @@
             React.PropTypes.shape({
                 word: React.PropTypes.string.isRequired,
                 code: React.PropTypes.number.isRequired,
+                freeWord: React.PropTypes.array.isRequired,
             })
         )
     },
+
+
+    getInitialState: function() {
+      return { freeWordsArr: [] };
+    },
+
+    freeWordsArrChanged: function(fwa) { 
+      console.log('WordsBlock: текст изменён - '+fwa); 
+      this.setState( [ {freeWord:fwa} ] );
+    },
+
     render: function() {
 
         var wordsCode = this.props.words.map(v =>
             React.DOM.div({ key: v.code, className: 'current_word' }, v.word, )
         );
 
-        var filterBlock =
+        //var inputWindow = 
+
+        //var filterBlock =
 
             return React.DOM.div({ className: 'wordsBlock' },
                 React.DOM.div({ className: 'Caption' }, this.props.caption),
