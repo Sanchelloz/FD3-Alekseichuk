27858939b7a3066d841b49e392dc58c588f973aa
Алексейчук Александр Николаевич <Alekseichuk.A@tbe.by>
2017-12-18 17:42:41 +0300

    Add sorted

--- filter/JS/filter.js
+++ filter/JS/filter.js
@@ -8,47 +8,82 @@
 
         catalog: React.PropTypes.arrayOf(
             React.PropTypes.shape({
-                word: React.PropTypes.string.isRequired,
+                words: React.PropTypes.string.isRequired,
                 code: React.PropTypes.number.isRequired,
                 freeWord: React.PropTypes.array.isRequired,
                 //workMode: React.PropTypes.number.isRequired,
+                wordContain: React.PropTypes.string.isRequired,
+                cbFreeWordTextChanged: React.PropTypes.string.isRequired,
+                sorted: React.PropTypes.boolean,
             })
         )
     },
 
+/*    freeWordTextChanged: function(EO) { 
+       
+      this.props.cbFreeWordTextChanged(EO.target.value);
+
+    },*/
 
     getInitialState: function() {
       return {
         freeWordsArr: this.props.words,
-        wordContain: 'ё',
+        wordContain: 'е',
+        sorted: false,
       };
     },
 
     processWords: function() {
+
       var filterArr = [];
+
+      
+      if (this.state.sorted) {
+
+        this.state.wordContain.sort(function (a, b) {
+          if (a.word > b.word) {
+            return 1;
+          }
+          if (a.word < b.word) {
+            return -1;
+          }
+            // a должно быть равным b
+            return 0;
+        });
+      }  
+
       for (i=0; i<this.props.words.length; i++){
 
-        if ( this.props.words[i].indexOf(this.state.wordContain) != -1 ){
+        if ( (this.props.words[i].word).indexOf(this.state.wordContain) != -1 ){
 
-          filterArr.push(wordContain)
+          filterArr.push(this.props.words[i])
 
         };
       }; 
         
-        this.setState( {freeWordsArr: filterArr} )
-      
+        this.setState( {freeWordsArr: filterArr, sorted: true} )
+     
     },
 
 
-    render: function() {
-        var wordContain 
-        var wordsCode = this.state.freeWordsArr.map(v =>
-            React.DOM.div({ key: v.code, className: 'current_word' }, v.word, )
-        );
 
-            return React.DOM.div({ className: 'wordsBlock' },
-                React.DOM.div({ className: 'Caption' }, this.props.caption),
-                React.DOM.div({ className: 'Catalog' }, wordsCode),
-            );
+    render: function() {
+
+      var wordsCode = this.state.freeWordsArr.map(v =>
+          React.DOM.div({ key: v.code, className: 'current_word' }, v.word, )
+      );
+
+      return React.DOM.div( { className: 'wordsBlock' },
+        React.DOM.div( { className: 'Caption' }, this.props.caption ),
+          React.DOM.label( {className:'WordsBlockFreeWord'},
+            React.DOM.input( {type:'text',name:'enterFreeWord',maxLength:"30",
+              className:'FreeWord', defaultValue:'', /*onChange:this.freeWordTextChanged,*/} ),
+            React.DOM.input( {type:'button', value:'Enter', className:'Btn',
+              onClick:this.processWords} ),
+            React.DOM.input( {type:'checkbox', value:'isSorted', name:'isSorted',
+              className:'chkBox', onClick:this.processWords} ),
+          ),
+        React.DOM.div({ className: 'Catalog' }, wordsCode ),
+      );
     },
 });
