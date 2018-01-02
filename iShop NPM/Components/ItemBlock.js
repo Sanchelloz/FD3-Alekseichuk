2d47b7fc80c44fc413125b27fbffc851f7119cd4
Sanchelloz <alekseichuk.a@gmail.com>
2018-01-02 15:15:20 +0300

    Add rowSelected

--- iShop NPM/Components/ItemBlock.js
+++ iShop NPM/Components/ItemBlock.js
@@ -5,42 +5,42 @@ import './../CSS/ItemBlock.css';
 
 class ItemBlock extends React.Component {
 
-  static propTypes = {
-    cbSelected: PropTypes.func.isRequired,
-    selectedRow: PropTypes.bool,
-    catalog: PropTypes.arrayOf(
-      PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        URL: PropTypes.string.isRequired,
-        code: PropTypes.number.isRequired,
-        price: PropTypes.number.isRequired,
-        residue: PropTypes.number.isRequired,
-      }),
-    ),
-  };
+    static propTypes = {
+        cbSelected: PropTypes.func.isRequired,
+        selectedRow: PropTypes.bool,
+        catalog: PropTypes.arrayOf(
+            PropTypes.shape({
+                name: PropTypes.string.isRequired,
+                URL: PropTypes.string.isRequired,
+                code: PropTypes.number.isRequired,
+                price: PropTypes.number.isRequired,
+                residue: PropTypes.number.isRequired,
+            }),
+        ),
+    };
 
-  rowClicked = (EO) => {
-    this.props.cbSelected(this.props.code);
+    rowClicked = (EO) => {
+        this.props.cbSelected(this.props.code);
 
-  };
+    };
 
-  render() {
+    render() {
 
-    return (
-    
-      <tr className={(this.props.selectedRow)?'Product SelectedRow':'Product'} onClick={this.rowClicked}>
-        <td className='Product_name'>{this.props.name}</td>
-        <td className='Product_foto'>
-          <img src={this.props.URL}/>
-        </td>
-        <td className='Product_count'>{this.props.code}</td>
-        <td className='Product_price'>{this.props.price}</td>
-        <td className='Product_residue'>{this.props.residue}</td>
-        <td className='Product_residue'><button>Редактировать</button></td>
-        <td className='Product_residue'><button>Удалить</button></td>
-      </tr>
-    )
-  }
+        return (
+
+            <tr className = {(this.props.selectedRow) ? 'Product SelectedRow' : 'Product'}
+							onClick = { this.rowClicked } >
+							<td className = 'Product_name' > { this.props.name } </td> <td className = 'Product_foto' >
+								<img src = { this.props.URL }/>
+							</td>
+							<td className = 'Product_count' > { this.props.code } </td>
+							<td className = 'Product_price'> { this.props.price } </td>
+							<td className = 'Product_residue'> { this.props.residue } </td>
+							<td className = 'Product_residue'> < button > Редактировать </button></td>
+							<td className = 'Product_residue'> < button > Удалить </button></td>
+            </tr>
+        )
+    }
 };
 
 export default ItemBlock;
--- ishop NPM/Components/CatalogBlock.js
+++ ishop NPM/Components/CatalogBlock.js
@@ -1,4 +1,4 @@
-import React from 'react';
+﻿import React from 'react';
 import PropTypes from 'prop-types';
 
 import './../CSS/CatalogBlock.css';
@@ -9,75 +9,66 @@ import ItemBlock from './ItemBlock';
 
 class CatalogBlock extends React.Component {
 
-  static propTypes = {
-    caption: PropTypes.string.isRequired,
-    //ModalWorkMode: PropTypes.number.isRequired,
-    selectedRowCode: PropTypes.number,
-    selectedRow: PropTypes.bool,
-    catalog: PropTypes.arrayOf(
-        PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            URL: PropTypes.string.isRequired,
-            code: PropTypes.number.isRequired,
-            price: PropTypes.number.isRequired,
-            residue: PropTypes.number.isRequired,
-        })
-    ),
-  };
-  
-  state = {
-    selectedRowCode: null,
-    ModalWorkMode: 0,
-  };
+    static propTypes = {
+        caption: PropTypes.string.isRequired,
+        //ModalWorkMode: PropTypes.number.isRequired,
+        selectedRowCode: PropTypes.number,
+        selectedRow: PropTypes.bool,
+        catalog: PropTypes.arrayOf(
+            PropTypes.shape({
+                name: PropTypes.string.isRequired,
+                URL: PropTypes.string.isRequired,
+                code: PropTypes.number.isRequired,
+                price: PropTypes.number.isRequired,
+                residue: PropTypes.number.isRequired,
+            })
+        ),
+    };
 
-  rowSelected = (code) => {
-    console.log('выбрана строка с кодом '+code);
-    this.setState( {selectedRowCode:code} );
-  };
+    state = {
+        selectedRowCode: null,
+        ModalWorkMode: 0,
+    };
 
-  render() {
+    rowSelected = (code) => {
+        console.log('выбрана строка с кодом ' + code);
+        this.setState({ selectedRowCode: code });
+    };
 
-    if (this.state.selectedRowCode == this.props.code) selectedRow=true;
-    
-    var catalogCode = this.props.catalog.map(v =>
-      <ItemBlock key={v.code}
-        name={v.name} URL={v.URL}
-        code={v.code} price={v.price}
-        residue={v.residue}
-        caption={this.props.caption}
-        cbSelected={this.rowSelected}
-        selectedRow
-      />
-    );
-    
-    
+    render() {
 
-    return (
-    <div className='CatalogBlock'>
-      <div className='Caption'> {this.props.caption} </div>
-      <div className='Catalog'>
-        <table>
-          <thead>
-            <tr>
-              <th>Наименование</th>
-              <th>Фото</th>
-              <th>Код товара</th>
-              <th>Цена, руб</th>
-              <th>Остаток на складе</th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>{catalogCode}</tbody>
-        </table>
-      </div>
-      <div className='NewBtn'>
-        <button>Добавить товар</button>
-      </div>
-    </div>
-    );
+        //if (this.state.selectedRowCode == this.props.code) selectedRow=true;
 
-  }
+        var catalogCode = this.props.catalog.map(v =>
+            <
+            ItemBlock key = { v.code }
+            name = { v.name }
+            URL = { v.URL }
+            code = { v.code }
+            price = { v.price }
+            residue = { v.residue }
+            caption = { this.props.caption }
+            cbSelected = { this.rowSelected }
+            selectedRow = { this.state.selectedRowCode == v.code }
+            />
+        );
+
+
+
+        return ( <div className = 'CatalogBlock' >
+          <div className = 'Caption' > { this.props.caption } < /div> <div className = 'Catalog' >
+            <table >
+							<thead >
+							<tr >
+							<th > Наименование < /th> <th > Фото < /th> <th > Код товара < /th> <th > Цена, руб < /th> <th > Остаток на складе < /th> <th > < /th> <th > < /th> < /tr >
+							</thead>
+							<tbody > { catalogCode } < /tbody>
+            </table > </div> <div className = 'NewBtn' >
+            <button > Добавить товар < /button> < /div >
+					</div>
+        );
+
+    }
 }
 
 export default CatalogBlock;
