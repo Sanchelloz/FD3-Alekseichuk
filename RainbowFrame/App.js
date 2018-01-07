a39c492d2c348ac9774dc8dbf9f43532c346a172
Sanchelloz <alekseichuk.a@gmail.com>
2018-01-07 23:05:24 +0300

    rainbow start

---
+++ RainbowFrame/App.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './components/RainbowFrame';
+
+let colorsArr = require("./Colors.json");
+
+const TEXT = 'RainbowFrame';
+
+ReactDOM.render(
+	<RainbowFrame
+		text = { TEXT }
+    colorsArr = { colorsArr }
+    />, document.getElementById('container')
+);
---
+++ RainbowFrame/Components/RainbowFrame.js
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './../CSS/RainbowFrame.css';
+
+
+class RainbowFrame extends React.Component {
+  static propTypes = {
+    colorsArr: PropTypes.array.isRequired,
+  };
+
+  render() {
+  	let colors = this.props.colorsArr
+
+  	for (let i=0; i<colors.length; i++){
+  		<div className = {'border_frame border_' + ( colors[i] )}></div>
+  	}
+
+  }
+}
+
+export default RainbowFrame;
